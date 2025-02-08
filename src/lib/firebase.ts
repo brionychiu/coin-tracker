4f52ac4f295c43e163cd79fdae16e277f05bcde1
brionychiu <yuikuta0618@gmail.com>
2025-02-08 13:24:41 +0800

    feat: firebase config

---
+++ src/lib/firebase.ts
@@ -0,0 +1,34 @@
+import { initializeApp } from "firebase/app";
+import { 
+  createUserWithEmailAndPassword, 
+  getAuth, 
+  onAuthStateChanged, 
+  signInWithEmailAndPassword, 
+  signOut 
+} from "firebase/auth";
+import type { User } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const registerUser = (email: string, password: string) =>
+  createUserWithEmailAndPassword(auth, email, password);
+
+const loginUser = (email: string, password: string) =>
+  signInWithEmailAndPassword(auth, email, password);
+
+const logoutUser = () => signOut(auth);
+
+const subscribeToAuthChanges = (callback: (user: User | null) => void) =>
+  onAuthStateChanged(auth, callback);
+
+export { auth, registerUser, loginUser, logoutUser, subscribeToAuthChanges };
