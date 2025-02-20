efb3e9bdecc247351dda92e06d9deeaf0eb3fcfb
brionychiu <yuikuta0618@gmail.com>
2025-02-20 22:13:49 +0800

    refactor(firebase): reorganize imports and improve sign-in functionality

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,13 +1,14 @@
-import { toast } from "sonner";
-import { initializeApp } from "firebase/app";
-import { 
-  createUserWithEmailAndPassword, 
-  getAuth, 
-  onAuthStateChanged, 
-  signInWithEmailAndPassword, 
-  signOut 
-} from "firebase/auth";
-import type { User } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import type { User } from 'firebase/auth';
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth';
+import Cookies from 'js-cookie';
+import { toast } from 'sonner';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -15,7 +16,7 @@ const firebaseConfig = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -23,19 +24,27 @@ const auth = getAuth(app);
 
 const signIn = async (email: string, password: string) => {
   try {
-    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password,
+    );
+    const token = await userCredential.user.getIdToken();
+    Cookies.set('authToken', token, { expires: 180, secure: true });
+
     return userCredential;
   } catch (error: any) {
-    console.error("登入失敗:", error.code, error.message);
+    console.error('登入失敗:', error.code, error.message);
 
     const errorMessages: Record<string, string> = {
-      "auth/invalid-email": "無效的電子郵件格式",
-      "auth/user-disabled": "此帳戶已被停用",
-      "auth/user-not-found": "找不到該使用者",
-      "auth/wrong-password": "密碼錯誤，請重新輸入",
-      "auth/too-many-requests": "嘗試次數過多，請稍後再試"
+      'auth/invalid-email': '無效的電子郵件格式',
+      'auth/user-disabled': '此帳戶已被停用',
+      'auth/user-not-found': '找不到該使用者',
+      'auth/wrong-password': '密碼錯誤，請重新輸入',
+      'auth/too-many-requests': '嘗試次數過多，請稍後再試',
     };
-    const errorMessage = errorMessages[error.code] || "登入失敗，請輸入正確信箱或密碼";
+    const errorMessage =
+      errorMessages[error.code] || '登入失敗，請輸入正確信箱或密碼';
     toast.error(errorMessage);
     return null;
   }
@@ -49,4 +58,4 @@ const logoutUser = () => signOut(auth);
 const subscribeToAuthChanges = (callback: (user: User | null) => void) =>
   onAuthStateChanged(auth, callback);
 
-export { auth, signIn, registerUser, logoutUser, subscribeToAuthChanges };
+export { auth, logoutUser, registerUser, signIn, subscribeToAuthChanges };
