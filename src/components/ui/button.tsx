aec404a67d1acb65e4c1bbba78e6ff81d4186cfe
brionychiu <yuikuta0618@gmail.com>
2025-03-20 23:17:55 +0800

    style(button): update ghost button variant styles

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -17,7 +17,7 @@ const buttonVariants = cva(
           'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
         secondary:
           'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        ghost: 'hover:bg-primary-02 hover:text-accent-foreground',
         link: 'text-gray-01 underline-offset-4 hover:underline',
       },
       size: {
