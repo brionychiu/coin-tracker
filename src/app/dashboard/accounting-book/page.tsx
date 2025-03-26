900366194eeb56e459283ea68c9ea3d6c3a494a5
brionychiu <yuikuta0618@gmail.com>
2025-03-26 18:06:38 +0800

    feat(accounting-book): enhance accounting book UI

--- src/app/dashboard/accounting-book/page.tsx
+++ src/app/dashboard/accounting-book/page.tsx
@@ -3,31 +3,47 @@
 import AddRecord from '@/app/dashboard/accounting-book/AddRecord';
 import { CustomCalendar } from '@/app/dashboard/accounting-book/CustomCalendar';
 import Records from '@/app/dashboard/accounting-book/Records';
+import { Button } from '@/components/ui/button';
+import { Plus } from 'lucide-react';
 import { useState } from 'react';
+
 export default function AccountingBookPage() {
+  const [isAdding, setIsAdding] = useState(false);
   const [date, setDate] = useState<Date | undefined>(new Date());
   const [month, setMonth] = useState<number>(
     date?.getMonth() ?? new Date().getMonth(),
   );
 
   const handleMonthChange = (newDate: Date) => {
-    setMonth(newDate.getMonth()); // 提取月份並更新狀態
+    setMonth(newDate.getMonth());
   };
 
   return (
-    <div className="w-full">
-      <div className="flex flex-row justify-center">
-        <CustomCalendar
-          mode="single"
-          selected={date}
-          onSelect={setDate}
-          onMonthChange={handleMonthChange} // 🔥 監聽月份變更
-          className="rounded-md border p-4 shadow"
-        />
-        <Records date={date} month={month} />
-      </div>
-      <div className="flex flex-row justify-center">
-        <AddRecord />
+    <div className="f-full mx-auto h-full rounded-2xl border bg-white shadow 2xl:max-w-6xl">
+      <div className="flex flex-row p-6">
+        <div className="flex w-96 flex-col items-center">
+          <CustomCalendar
+            mode="single"
+            selected={date}
+            onSelect={setDate}
+            onMonthChange={handleMonthChange}
+            className="p-4"
+          />
+          <Button onClick={() => setIsAdding(true)}>
+            <Plus />
+            新增記帳
+          </Button>
+        </div>
+        <div className="flex-1">
+          {isAdding ? (
+            <AddRecord
+              onCancel={() => setIsAdding(false)}
+              onSave={() => setIsAdding(false)}
+            />
+          ) : (
+            <Records date={date} month={month} />
+          )}
+        </div>
       </div>
     </div>
   );
