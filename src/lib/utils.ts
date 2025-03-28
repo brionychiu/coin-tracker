e059ce68032dfca1f6fea90ea3e1f93873fa07de
brionychiu <yuikuta0618@gmail.com>
2025-03-28 16:04:09 +0800

    feat(utils): add extractFileName utility function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const extractFileName = (url: string): string => {
+  return decodeURIComponent(url.split('/').pop()?.split('?')[0] || '未知檔名');
+};
