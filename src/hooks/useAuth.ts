06f569bb916f05e625a492df6a2c9f06420d8919
brionychiu <yuikuta0618@gmail.com>
2025-03-31 18:11:21 +0800

    feat(hooks): add router navigation on logout

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,14 +1,18 @@
+import Cookies from 'js-cookie';
+import { useRouter } from 'next/navigation';
+
 import { logoutUser } from '@/lib/api/auth';
 import { useAuthStore } from '@/stores/authStore';
-import Cookies from 'js-cookie';
 
 export function useAuth() {
+  const router = useRouter();
   const { user, isLoading, setUser } = useAuthStore();
 
   const logout = async () => {
     await logoutUser();
     Cookies.remove('authToken');
     setUser(null);
+    router.push('/');
   };
 
   return {
