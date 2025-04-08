2d3a51c0f6624a920fffc4fc92de48c5cb5d907c
brionychiu <yuikuta0618@gmail.com>
2025-04-08 15:51:35 +0800

    refactor(utils): remove unused extractFileName function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,7 +4,3 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export const extractFileName = (url: string): string => {
-  return decodeURIComponent(url.split('/').pop()?.split('?')[0] || '未知檔名');
-};
