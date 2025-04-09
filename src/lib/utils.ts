4bfb9de9d278e30383f7c79d0326f1cece85c935
brionychiu <yuikuta0618@gmail.com>
2025-04-09 22:18:02 +0800

    feat(utils): add getAccountLabel function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
+import { accountOptions } from '@/lib/accountOptions';
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const getAccountLabel = (value: string): string => {
+  return accountOptions.find((option) => option.value === value)?.label || '未知帳戶';
+};
