3e295e76a407e921ea6a8dfd18e627f882558698
brionychiu <yuikuta0618@gmail.com>
2025-04-10 18:17:52 +0800

    style(layout): remove unnecessary margin from footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-02 mt-8 py-4 text-center text-white">
+    <footer className="bg-gray-02 py-4 text-center text-white">
       <p>&copy; 2025 快樂記帳 版權所有.</p>
     </footer>
   );
