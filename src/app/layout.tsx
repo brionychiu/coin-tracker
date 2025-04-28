3c505cd13c23eb6270e20c1d00f9ecf735aa3633
brionychiu <yuikuta0618@gmail.com>
2025-04-28 11:36:09 +0800

    feat(layout): ensure footer stays at the bottom of the page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,8 +35,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
-        <Footer />
+        <div className="flex min-h-screen flex-col">
+          <main className="flex-grow">{children}</main>
+          <Footer />
+        </div>
         <Toaster />
       </body>
     </html>
