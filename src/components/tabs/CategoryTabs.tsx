39ddbb93a786766dfca088d996c25a671007c007
brionychiu <yuikuta0618@gmail.com>
2025-05-05 15:18:18 +0800

    feat(tabs): add edit functionality to CategoryTabs, integrate AddCategoryDialog for adding categories

--- src/components/tabs/CategoryTabs.tsx
+++ src/components/tabs/CategoryTabs.tsx
@@ -1,10 +1,13 @@
+import { CirclePlus, Trash2 } from 'lucide-react';
+import { useState } from 'react';
+
+import AddCategoryDialog from '@/components/modal/AddCategory';
 import { Button } from '@/components/ui/button';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { EXPENSE_CATEGORIES, INCOME_CATEGORIES } from '@/lib/categories';
-import { CirclePlus } from 'lucide-react';
-import { useState } from 'react';
 
 interface TabsCategoryProps {
+  isEdit: boolean;
   value?: string;
   onChange?: (value: string) => void;
 }
@@ -19,27 +22,34 @@ const TabButton = ({
   const bgColor = activeTab === 'expenses' ? 'bg-red-04' : 'bg-green-01';
 
   return (
-    <Button
-      variant="tabHover"
-      className="flex flex-col items-center gap-1"
-      type="button"
-      onClick={onClick}
-    >
-      <span className="relative flex h-10 w-10 items-center justify-center">
-        {isSelected && (
-          <span
-            className={`absolute left-0 right-0 top-1 mx-auto h-8 w-8 rounded-full ${bgColor} opacity-80`}
-          />
-        )}
-        <Icon className="relative z-10 size-6" />
-      </span>
-      <p className="z-10 text-xs font-normal">{label}</p>
-    </Button>
+    <div className="relative">
+      <Button
+        variant="tabHover"
+        className="flex flex-col items-center gap-1"
+        type="button"
+        onClick={onClick}
+      >
+        <span className="relative flex h-10 w-10 items-center justify-center">
+          {isSelected && (
+            <span
+              className={`absolute left-0 right-0 top-1 mx-auto h-8 w-8 rounded-full ${bgColor} opacity-80`}
+            />
+          )}
+          <Icon className="relative z-10 size-6" />
+        </span>
+        <p className="z-10 text-xs font-normal">{label}</p>
+      </Button>
+    </div>
   );
 };
 
-export default function CategoryTabs({ value, onChange }: TabsCategoryProps) {
+export default function CategoryTabs({
+  isEdit,
+  value,
+  onChange,
+}: TabsCategoryProps) {
   const [activeTab, setActiveTab] = useState<'expenses' | 'income'>('expenses');
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
 
   const categories =
     activeTab === 'expenses' ? EXPENSE_CATEGORIES : INCOME_CATEGORIES;
@@ -50,6 +60,12 @@ export default function CategoryTabs({ value, onChange }: TabsCategoryProps) {
       tab === 'expenses' ? EXPENSE_CATEGORIES : INCOME_CATEGORIES
     )[0];
     onChange?.(firstCategory.label);
+  };
+
+  const handleDelete = () => {
+    if (value && value !== 'add') {
+      console.log('刪除項目:', value);
+    }
   };
 
   return (
@@ -79,21 +95,42 @@ export default function CategoryTabs({ value, onChange }: TabsCategoryProps) {
                   isSelected={value === label}
                   activeTab={activeTab}
                   onClick={() => onChange?.(label)}
+                  isEdit={isEdit}
                 />
               ))}
-              <TabButton
-                label="新增"
-                icon={CirclePlus}
-                isSelected={value === 'add'}
-                activeTab={activeTab}
-                onClick={() => {
-                  onChange?.('add');
-                }}
-              />
             </div>
           </TabsContent>
         ))}
       </Tabs>
+      {isEdit && (
+        <div className="mt-6 flex items-center justify-end gap-4 sm:mt-0">
+          <Button
+            type="button"
+            variant="iconHover"
+            size="icon"
+            onClick={() => {
+              setIsDialogOpen(true);
+            }}
+          >
+            <CirclePlus className="size-5" />
+          </Button>
+          <Button
+            type="button"
+            variant="iconHover"
+            size="icon"
+            onClick={handleDelete}
+          >
+            <Trash2 className="size-5" />
+          </Button>
+        </div>
+      )}
+      {isEdit && (
+        <AddCategoryDialog
+          open={isDialogOpen}
+          onOpenChange={setIsDialogOpen}
+          type={activeTab}
+        />
+      )}
     </div>
   );
 }
