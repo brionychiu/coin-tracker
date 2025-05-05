9cde73675a6986dfcc5a3a9aa8c79d3073ce923a
brionychiu <yuikuta0618@gmail.com>
2025-05-05 22:53:02 +0800

    feat(api): add endpoint to get visible categories, implement getVisibleCategories function to fetch system and user categories

---
+++ src/app/api/categories/route.ts
@@ -0,0 +1,63 @@
+import {
+  collection,
+  db,
+  getDocs,
+  orderBy,
+  query,
+  where,
+} from '@/lib/firebase';
+import { Category } from '@/types/category';
+import { NextResponse } from 'next/server';
+
+export async function getVisibleCategories(uid: string | undefined, type: 'income' | 'expenses') {
+  if (!uid) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  }
+  // 1. 取得 system 預設類別
+  const systemQuery = query(
+    collection(db, 'categories'),
+    where('type', '==', type),
+    where('createdBy', '==', 'system'),
+    orderBy('createTime', 'asc'), 
+  );
+
+  // 2. 取得使用者自訂類別
+  const userQuery = query(
+    collection(db, 'categories'),
+    where('type', '==', type),
+    where('createdBy', '==', uid),
+    orderBy('createTime', 'asc'), 
+  );
+
+  const [systemSnap, userSnap] = await Promise.all([
+    getDocs(systemQuery),
+    getDocs(userQuery),
+  ]);
+
+  const systemCategories = systemSnap.docs
+    .map((doc) => ({ id: doc.id, label: doc.data().label,  
+      icon: doc.data().icon,    
+      type: doc.data().type,
+      createTime: doc.data().createTime,    
+      createdBy: doc.data().createdBy, 
+      deletedBy: doc.data().deletedBy,}))
+    .filter((cat: any) => !cat.deletedBy?.includes(uid));
+
+  const userCategories = userSnap.docs
+  .map((doc) => ({
+    id: doc.id,
+    label: doc.data().label, 
+    icon: doc.data().icon,   
+    createTime: doc.data().createTime,
+    type: doc.data().type,   
+    createdBy: doc.data().createdBy, 
+    deletedBy: doc.data().deletedBy, 
+  }));
+
+  const categories: Category[] = [
+    ...systemCategories,
+    ...userCategories,
+  ];
+
+  return categories;
+}
