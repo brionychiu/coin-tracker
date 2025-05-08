2fb0866f77491b027984ec29922a661b3cbfb0c4
brionychiu <yuikuta0618@gmail.com>
2025-05-08 14:21:58 +0800

    feat(hooks): add useAccountMap custom hook

---
+++ src/hooks/useAccountMap.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+
+import { useAuth } from '@/hooks/useAuth';
+import { getAccountMap } from '@/lib/api/account';
+import { Account } from '@/types/account';
+
+export function useAccountMap() {
+  const { uid } = useAuth();
+  const [accountMap, setAccountMap] = useState<Record<string, Account>>({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchAccountMap = async () => {
+      if (!uid) {
+        console.error('使用者未登入，無法取得分類');
+        setLoading(false);
+        return;
+      }
+
+      const map = await getAccountMap(uid);
+      setAccountMap(map);
+      setLoading(false);
+    };
+
+    fetchAccountMap();
+  }, [uid]);
+
+  return { accountMap, loading };
+}
