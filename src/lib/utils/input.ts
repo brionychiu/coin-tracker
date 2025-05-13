bbb45ef0129ced98d901bcd10b313ce8591c5b4d
brionychiu <yuikuta0618@gmail.com>
2025-05-13 15:19:29 +0800

    feat(utils): introduce parseCurrencyValue function to handle currency parsing

--- src/lib/utils/input.ts
+++ src/lib/utils/input.ts
@@ -1,5 +1,15 @@
-export const handleNumericInput = (e: React.KeyboardEvent<HTMLInputElement>) => {
-  if (!/\d/.test(e.key) && !['Backspace', 'ArrowLeft', 'ArrowRight', 'Delete'].includes(e.key)) {
+export const handleNumericInput = (
+  e: React.KeyboardEvent<HTMLInputElement>,
+) => {
+  if (
+    !/\d/.test(e.key) &&
+    !['Backspace', 'ArrowLeft', 'ArrowRight', 'Delete'].includes(e.key)
+  ) {
     e.preventDefault();
   }
 };
+
+export function parseCurrencyValue(fullValue: string) {
+  const [group, value] = fullValue.split('-');
+  return { group, value };
+}
