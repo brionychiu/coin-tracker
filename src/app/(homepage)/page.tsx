0c6f633b9dbd0d473b5ae0725b6ee049b91526f9
brionychiu <yuikuta0618@gmail.com>
2025-05-14 18:52:59 +0800

    feat(homepage): add HomeHero component to homepage layout

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -1,15 +1,15 @@
 import Navbar from '@/components/layout/Navbar';
+import { HomeHero } from '@/components/sections/HomeHero';
 
 export default function HomePage() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+    <div className="flex flex-col font-[family-name:var(--font-geist-sans)]">
       <header>
         <Navbar />
       </header>
-      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-        未登入的首頁
+      <main className="flex flex-grow flex-col">
+        <HomeHero />
       </main>
-      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6"></footer>
     </div>
   );
 }
